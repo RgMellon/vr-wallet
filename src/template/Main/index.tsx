878e68c42b508eabdfc7d2dd17e6e79ad066bfdf
Renan <renan_gian@hotmail.com>
2023-05-12 14:53:49 -0300

    feat: creating Main theme

---
+++ src/template/Main/index.tsx
@@ -0,0 +1,41 @@
+import * as S from "./styles";
+
+import upDetailBackground from "../../assets/img/background/up.png";
+import downDetailBackground from "../../assets/img/background/down.png";
+import { Image } from "react-native";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function Main({ children }: Props) {
+  return (
+    <S.Container>
+      <Image
+        source={upDetailBackground}
+        resizeMode="contain"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: -30,
+          right: 0,
+          width: 350,
+          height: 236,
+        }}
+      />
+      {children}
+      <Image
+        source={downDetailBackground}
+        resizeMode="contain"
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: 350,
+          height: 236,
+          left: 100,
+          right: 0,
+        }}
+      />
+    </S.Container>
+  );
+}
